de60ac02e3e429ae3fb7300745022b1df5649fd1
0x1uke <luke@0x1uke.com>
2024-04-23 20:28:32 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Cyfrin Updraft Foundry Fundamentals
+
+## Topics Learned
+
+* Foundry
+* Writing unit and integration tests for smart contracts
+* Scripting smart contract deployments against multiple blockchains
+* Integrating Chainlink's Price Feeds
+* Creating mock contract for local Anvil blockchain testing
+* Calculating and optimizing gas costs
+* Storage optimization
+* Simplify Foundry commands with MakeFile
---
+++ src/FundMe.sol
@@ -0,0 +1,95 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.25;
+
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "./PriceConverter.sol";
+
+error FundMe__NotOwner();
+
+contract FundMe {
+    using PriceConverter for uint256;
+
+    mapping(address funder => uint256 amountFunded) private s_addressToAmountFunded;
+    address[] private s_funders;
+
+    address private immutable i_owner;
+    uint256 public constant MINIMUM_USD = 5e18;
+    AggregatorV3Interface private s_priceFeed;
+
+    constructor(address priceFeed) {
+        i_owner = msg.sender;
+        s_priceFeed = AggregatorV3Interface(priceFeed);
+    }
+
+    function fund() public payable {
+        require(
+            msg.value.getConversionRate(s_priceFeed) >= MINIMUM_USD,
+            "Didn't send enough ETH"
+        );
+        s_funders.push(msg.sender);
+        s_addressToAmountFunded[msg.sender] += msg.value;
+    }
+
+    function withdraw() public onlyOwner {
+        uint256 fundersLength = s_funders.length;
+
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < fundersLength;
+            funderIndex++
+        ) {
+            address funder = s_funders[funderIndex];
+            s_addressToAmountFunded[funder] = 0;
+        }
+        s_funders = new address[](0);
+
+        // transfer
+        //payable(msg.sender).transfer(address(this).balance);
+
+        // send
+        //bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        //require(sendSuccess, "Send failed");
+
+        // call
+        (bool callSuccess, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
+        require(callSuccess, "Call failed");
+    }
+
+    function getVersion() public view returns (uint256) {
+        return s_priceFeed.version();
+    }
+
+    modifier onlyOwner() {
+        // require(msg.sender == i_owner, "Sender is not owner");
+        if (msg.sender != i_owner) {
+            revert FundMe__NotOwner();
+        }
+        _;
+    }
+
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
+    }
+
+    // View/Pure (Get) Functions
+    function getAddresstoAmountFunded(
+        address fundingAddress
+    ) external view returns(uint256) {
+        return s_addressToAmountFunded[fundingAddress];
+    }
+
+    function getFunder(uint256 index) external view returns(address) {
+        return s_funders[index];
+    }
+
+    function getOwner() external view returns(address) {
+        return i_owner;
+    }
+}
